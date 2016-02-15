5949b65cbda0a1f0eb149faa915f8df0823f6d9c
unknown <jessetius@gmail.com>
2016-02-15 15:01:37 +0200

    V2 changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 import sys
 
@@ -17,12 +18,27 @@ class FizzBuzz():
 
     # Run from 1 to "end". Maybe. Test fails for some reason
     def run(self, end, out=sys.stdout):
-        for i in range(1, end):
+        for i in range(0, end):
             print >> out, self.calc(i)
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
-        return i
+        
+        prime_numbers = [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,97];
+
+        if (i in prime_numbers):
+            str = "%d is a prime" %i
+            return str
+        else:
+            if ((i % 15) == 0):
+                return "FizzBuzz"
+            elif ((i % 5) == 0):
+                return "Buzz"
+            elif ((i % 3) == 0):
+                return "Fizz"
+            else:
+                return i
+        
 
 if __name__ == "__main__":
     app = FizzBuzz()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 import unittest
 import sys
 from StringIO import StringIO
@@ -20,6 +21,45 @@ class TestSuite(unittest.TestCase):
 
         self.failIf(len(output.getvalue().splitlines()) != 100)
 
+    def test_prime(self):
+        app = FizzBuzz()
+
+        prime_numbers = [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,97];
+
+        for i in prime_numbers:
+            self.failIf(app.calc(i) != "%d is a prime" % i)
+      
+
+    def test_fizz(self):
+        app = FizzBuzz()
+
+        self.failIf(app.calc(6) != "Fizz")
+        self.failIf(app.calc(9) != "Fizz")
+        self.failIf(app.calc(12) != "Fizz")
+
+    def test_buzz(self):
+        app = FizzBuzz()
+
+        self.failIf(app.calc(10) != "Buzz")
+        self.failIf(app.calc(20) != "Buzz")
+        self.failIf(app.calc(25) != "Buzz")
+
+
+    def test_fizzbuzz(self):
+        app = FizzBuzz()
+			
+        self.failIf(app.calc(15) != "FizzBuzz")
+        self.failIf(app.calc(30) != "FizzBuzz")
+        self.failIf(app.calc(45) != "FizzBuzz")
+		
+    def test_number(self):
+        app = FizzBuzz()
+		
+        self.failIf(app.calc(2) != 2)
+        self.failIf(app.calc(4) != 4)
+        self.failIf(app.calc(8) != 8)
+		
+		
 def main():
     unittest.main()
 
